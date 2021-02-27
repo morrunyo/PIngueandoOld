e74266b1aeea46ae0df8c3cefd44443e24c954b2
Mateo Soto Ramos <mateo.soto@grupofrinsa.com>
2021-02-27 11:19:34 +0100

    Generating models

--- PingueandoWeb/PIngueandoWebApp/models.py
+++ PingueandoWeb/PIngueandoWebApp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Pinguer(models.Model):
+    name = models.CharField(max_length=128)
+
+class PinguerEvent(models.Model):
+    EVENT_TYPE = [
+    (1, 'Activation'),
+    (2, 'Deactivation'),
+    ]
+    pinguer=models.ForeignKey(Pinguer, on_delete=models.CASCADE )
+    event_type = models.IntegerField(max_length=1, choices=EVENT_TYPE)
+    event_date = models.DateField()
